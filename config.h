9006c0c0703c656194ff2928f23924b8e88a2968
Rudy Dellomas III <rdellomasiii@outlook.com>
2019-11-22 11:12:55 +1000

    [PATCH] A hack that lets me edit a bash script
    Bound MODKEY+Q to a dmenu script that lets me edit a bash script.
    This script is ran whenever the ThinkVantage key is pressed via
    xbindkeys.

--- config.h
+++ config.h
@@ -96,12 +96,14 @@ static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *networkmancmd[] = { "networkmanager_dmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", color0, "-nf", color7, "-sb", color10, "-sf", color15, NULL };
+static const char *bindcmd[] = { "dmenu-reprogram", "-m", dmenumon, "-fn", dmenufont, "-nb", color0, "-nf", color7, "-sb", color10, "-sf", color15, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
     { 0,                            HOLDKEY,   showalttag,       {0} },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
     { MODKEY,                       XK_n,      spawn,          {.v = networkmancmd} },
+    { MODKEY,                       XK_q,      spawn,          {.v = bindcmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
