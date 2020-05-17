e6c6384369ed311f4ebb620916d4707864e44141
Rudy Dellomas III <rdellomasiii@outlook.com>
2020-05-17 14:08:06 +1000

    Added Pico-8 rule.

--- config.h
+++ config.h
@@ -64,6 +64,7 @@ static const Rule rules[] = {
     { "cura",               NULL,       NULL,           1 << 4,            0,           -1 },
     { "Steam",              NULL,       NULL,           1 << 6,            1,           -1 },
     { "Steam",              "Steam",   "Steam",         1 << 6,            0,           -1 },
+    { "pico8",              "pico8",    NULL,         1 << 6,            0,           -1 },
     { "Lutris",             NULL,       NULL,           1 << 6,            0,           -1 },
 	{ "Emacs",              NULL,       NULL,           1 << 0,            0,           -1 },
 };
