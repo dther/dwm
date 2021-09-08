4b05e734274fb680df2338cca7d5ab8fc4654212
Rudy Dellomas III <rdellomasiii@outlook.com>
2021-09-08 13:23:23 +1000

    Made borderpx larger, hard to see on some systems

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
 /* See LICENSE file for copyright and license details. */ 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;	/* gaps for A E S T H E T I C */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
