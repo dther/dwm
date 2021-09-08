aa9acd1a13e30b174f05a3bc7a80f3bd40db6b40
Rudy Dellomas III <rdellomasiii@outlook.com>
2021-09-08 14:16:34 +1000

    Altered focusurgent to search all monitors for urgency

--- focusurgent.c
+++ focusurgent.c
@@ -1,15 +1,26 @@
 static void
 focusurgent(const Arg *arg) {
 	Client *c;
+	Monitor *m;
+	/* Prioritise this monitor's urgent clients first */
 	for(c=selmon->clients; c && !c->isurgent; c=c->next);
-	if(c) {
+	if(!c) {
+		// TODO: test this- should search other monitors for urgent clients
+		for(m=mons; m && !c; m=m->next) {
+			if (m == selmon) continue; /* Skip current monitor - already checked */
+			for(c=m->clients; c && !c->isurgent; c=c->next);
+		}
+	}
+
+	if (c) {
+		selmon = c->mon;
 		if(!ISVISIBLE(c)) {
-			const Arg a = {.ui = (c->tags | c->mon->tagset[c->mon->seltags])};
+			const Arg a = {.ui = (c->tags | selmon->tagset[selmon->seltags])};
 			setws(c->ws);
-			view(&a);
+			if(!ISVISIBLE(c)) view(&a);
 		}
 		if(ISVISIBLE(c))
 				focus(c);
-        restack(selmon);
+		restack(selmon);
 	}
 }
